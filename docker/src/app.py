52e377b5730b002db50240cfb4073916cdcdf85b
lazynooblet <1316058+lazynooblet@users.noreply.github.com>
2023-06-23 09:40:36 +0100

    sleep for t seconds

--- docker/src/app.py
+++ docker/src/app.py
@@ -113,6 +113,6 @@ while True:
     # wait until next iteration
     t = randint(settings['APP_MIN_SLEEP'], settings['APP_MAX_SLEEP'])
     logging.info(f'Next wake at {str(datetime.fromtimestamp(time() + t))} ({t}s)')
-    sleep(random.randint())
+    sleep(t)
 
 logging.info('Exiting')
